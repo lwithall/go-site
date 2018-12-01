5297aadfa6eff68a32c5e3b7c460b9a018586656
Luke Withell <lukewithell@hotmail.com>
2018-12-01 21:55:25 +1100

    Adding initial commit.

--- server.go
+++ server.go
@@ -1,15 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+   "net/http"
+   "time"
+   "html/template"
+   "log"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World")
+
+type Welcome struct {
+   Title string
+   Name string
+   Time string
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+    welcome := Welcome{"Login", "Anonymous", time.Now().Format(time.Stamp)}
+
+    templates := template.Must(template.ParseFiles("login.html"))
+    if err := templates.ExecuteTemplate(w, "login.html", welcome); err != nil {
+       http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", login)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+/*func main() {
+   welcome := Welcome{"Login", "Anonymous", time.Now().Format(time.Stamp)}
+
+   templates := template.Must(template.ParseFiles("login.html"))
+
+   http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
+
+      if name := r.FormValue("name"); name != "" {
+         welcome.Name = name;
+      }
+      if err := templates.ExecuteTemplate(w, "login.html", welcome); err != nil {
+         http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
+   })
+
+   //Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+   //Print any errors from starting the webserver using fmt
+   fmt.Println("Listening");
+   fmt.Println(http.ListenAndServe(":8080", nil));
+}*/
